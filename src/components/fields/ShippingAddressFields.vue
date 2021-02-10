835e4c56ae972b80b9d350e32806a9e26556a2af
Pier <pier@intersezione.com>
2021-02-10 11:13:23 +0100

    ShippingAddressFields - added phone hint for carrier needs

--- src/components/fields/ShippingAddressFields.vue
+++ src/components/fields/ShippingAddressFields.vue
@@ -113,6 +113,7 @@
           <v-text-field
             id="shipping-address-phone"
             :label="inputLabel('phone')"
+            :hint="inputLabel('phone_hint')"
             v-model="phone"
             :error-messages="errorMessages('phone')"
             @input="handleInput()"
